a6fe059de2aca2e6f3ccffbbab32005451396d09
Pratik Bhirud <46158725+Pratik0247@users.noreply.github.com>
2025-04-11 15:35:36 -0600

    BDA-36: Makes music splitter K8s ready, adds terraform deployments for splitter and rabbitmq (#21)

--- shared/constants.py
+++ shared/constants.py
@@ -2,8 +2,9 @@ import os
 
 # RabbitMQ related constants
 RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
-RABBITMQ_USER = os.getenv('RABBITMQ_USER', 'guest')
-RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD', 'guest')
+RABBITMQ_PORT = 5672
+RABBITMQ_USER = os.getenv('RABBITMQ_USER', 'user')
+RABBITMQ_PASS = os.getenv('RABBITMQ_PASS', 'password')
 
 GCS_BUCKET_NAME = 'bda-media-bucket'
 SPLIT_QUEUE_NAME = "split-jobs"
